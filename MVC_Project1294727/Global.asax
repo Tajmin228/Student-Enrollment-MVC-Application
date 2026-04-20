b27e4f3e89fc1fe93c758198620f9c37c07c6438
Tajmin228 <138378119+Tajmin228@users.noreply.github.com>
2026-04-20 09:20:27 +0600

    Add files via upload

---
+++ MVC_Project1294727/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Project1294727.MvcApplication" Language="C#" %>
---
+++ MVC_Project1294727/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Project1294727.Startup))]
+namespace MVC_Project1294727
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
